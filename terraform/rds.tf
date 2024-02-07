05dcf57758f0e74f0933caed75df8e9a3afdbfc1
Luke-Bradford-BBD <luke.bradford@bbd.co.za>
2024-02-07 18:55:40 +0200

    CRAF-10: Parameter tweaking and param group for db
    - Parameter group had to be added to allow connecting to the DB

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,6 +1,8 @@
 resource "aws_db_instance" "postgres-dev" {
   allocated_storage = 20 # Minimum as far as I can tell ()
-  db_name           = "Craft-Beer-dev"
+
+  identifier_prefix = "craft-beer-db-"
+  db_name           = "craft_beer_db"
   engine            = "postgres"
   engine_version    = var.postgres_version # Latest version of PostgreSQL available on AWS RDS (as of 2024-02-06)
   instance_class    = "db.t3.micro"
@@ -14,5 +16,19 @@ resource "aws_db_instance" "postgres-dev" {
 
   # Networking
   vpc_security_group_ids = [aws_security_group.craft_beer_rds_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.craft_beer_subnet_group.name
+  # db_subnet_group_name   = aws_db_subnet_group.craft_beer_subnet_group.name
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+
+  parameter_group_name = aws_db_parameter_group.craft_beer_pg_parameter_group.name
+}
+
+
+resource "aws_db_parameter_group" "craft_beer_pg_parameter_group" {
+  name   = "rds-pg"
+  family = "postgres16"
+
+  parameter {
+    name  = "rds.force_ssl"
+    value = "0"
+  }
 }
