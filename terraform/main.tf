6e3ecc2c8665bea55c6ab375b381c5a6aea489ca
Luke-Bradford-BBD <luke.bradford@bbd.co.za>
2024-02-11 10:17:29 +0200

    CRAF-10: Added remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+  backend "s3" {
+    bucket         = "craft-beer-db-terraform-backend"
+    key            = "craftdb/rds/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "craft-beer-db-terraform-lock"
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
   default_tags {
