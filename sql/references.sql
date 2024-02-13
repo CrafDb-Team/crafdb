47d6879c9e70ed0838878f28cefa5b481fe7a720
Ryan Trickett <ryan.trickett@bbd.co.za>
2024-02-13 09:06:25 +0200

    Added Liquibase Syntax for References

--- sql/references.sql
+++ sql/references.sql
@@ -1,8 +1,16 @@
+-- liquibase formatted sql
+
+-- changeset rtrickett:keg-batch-fk
 ALTER TABLE Keg 
 ADD CONSTRAINT Keg_BatchID_FK FOREIGN KEY (BatchID) REFERENCES Batch (BatchID);
+-- rollback ALTER TABLE Keg DROP CONSTRAINT Keg_BatchID_FK
 
+-- changeset rtrickett:keg-batch-fk
 ALTER TABLE Batch 
 ADD CONSTRAINT Batch_BeerID_FK FOREIGN KEY (BeerID) REFERENCES Beer (BeerID);
+-- rollback ALTER TABLE Batch DROP CONSTRAINT Batch_BeerID_FK
 
+-- changeset rtrickett:keg-batch-fk
 ALTER TABLE Batch 
 ADD CONSTRAINT Batch_BatchStatusID_FK OREIGN KEY (BatchStatusID) REFERENCES BatchStatus (BatchStatusID);
+-- rollback ALTER TABLE Batch DROP CONSTRAINT Batch_BatchStatusID_FK
