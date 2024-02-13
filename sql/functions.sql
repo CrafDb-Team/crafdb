0ae48cc757ca623ee31f7679df6260d2bd0713b1
Mamphane Edwin Mokoena <mamphane@bbd.co.za>
2024-02-13 17:15:55 +0200

    Changed function to count on beer not batch

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,23 +1,24 @@
 --liquibase formatted sql
 
---changeset emokoena:create-count-unsold-kegs-from-batch
-CREATE FUNCTION CountUnsoldKegsFromBatch(batch_id INT)
+--changeset emokoena:create-count-unsold-kegs-of-
+CREATE FUNCTION CountUnsoldKegsOfBeer(beer_id INT)
 RETURNS INT
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    remaining_kegs_from_batch INTEGER;
+    remaining_kegs_of_beer INTEGER;
 BEGIN
     SELECT count(*) 
-    INTO remaining_kegs_from_batch
-    FROM Batch bt
+    INTO remaining_kegs_of_beer
+    FROM Beer br
+    INNER JOIN Batch bt on bt.BatchID = br.BatchID
     INNER JOIN KEG kg on bt.BatchID = kg.BatchID
     LEFT JOIN OrderItem oi on kg.KegID = oi.KegID
     WHERE bt.BatchID = batch_id
     AND oi.OrderID IS NULL;
     
-    RETURN remaining_kegs_from_batch;
+    RETURN remaining_kegs_of_beer;
 END;
 $$;
--- rollback DROP FUNCTION IF EXISTS remaining_kegs_from_batch(INTEGER);
+-- rollback DROP FUNCTION IF EXISTS CountUnsoldKegsOfBeer(INTEGER);
