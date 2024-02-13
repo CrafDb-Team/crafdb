c3fd843a6bd89d84cdb1dd6465504ef2a72f2a20
LucindaBotes <Lucinda.Botes@bbd.co.za>
2024-02-13 16:39:20 +0200

    CRAF-20: Remove quatation marks

--- sql/references.sql
+++ sql/references.sql
@@ -2,6 +2,6 @@
 
 --changeset ljordaan:alter-table-beer-table
 ALTER TABLE Beer
-ADD CONSTRAINT Beer_BeerTypeID_FK FOREIGN KEY ("BeerTypeID")
-  REFERENCES BeerType("BeerTypeID");
+ADD CONSTRAINT Beer_BeerTypeID_FK FOREIGN KEY (BeerTypeID)
+  REFERENCES BeerType(BeerTypeID);
 -- rollback ALTER TABLE Beer DROP CONSTRAINT Beer_BeerTypeID_FK
