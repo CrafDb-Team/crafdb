ebba5a90d558135b845fa824d555486c464e6d8d
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-02-13 16:39:52 +0200

    Fix: columns should not be NULLABLE

--- sql/order-items.sql
+++ sql/order-items.sql
@@ -3,15 +3,15 @@
 -- changeset batandwa:order-items-table-creation
 CREATE TABLE OrderItem (
   OrderItemID serial PRIMARY KEY,
-  OrderID integer,
-  KegID integer,
-  OrderItemStateID integer,
+  OrderID integer NOT NULL,
+  KegID integer NOT NULL,
+  OrderItemStateID integer NOT NULL,
   PriceAtSale numeric NOT NULL
 );
 
 CREATE TABLE OrderItemState (
   OrderItemStateID serial PRIMARY KEY,
-  OrderState varchar(32)
+  OrderState varchar(32) NOT NULL
 );
 
 ALTER TABLE OrderItem
