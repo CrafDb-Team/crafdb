86417a004d1d8232f10e65665eb70488c037aa18
EdwinToTheBatCave <mamphane@bbd.co.za>
2024-02-14 13:15:40 +0200

    Fixed insert statement

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -17,14 +17,23 @@ BEGIN
     OrderItemStateID integer NOT NULL,
     PriceAtSale numeric NOT NULL
     */
-    INSERT INTO OrderItem oi
+    INSERT INTO OrderItem oi (OrderID, KegID, OrderItemStateID, PriceAtSale)
     SELECT 
         order_id,
+        kg.KegID,
         0,
         br.PricePerKeg
     FROM Keg kg
     INNER JOIN Beer br on br.BeerID = kg.BeerID
-    FETCH FIRST quantity ROWS ONLY;
+    WHERE NOT EXISTS (
+        SELECT 
+            KegID, 
+            OrderItemStateID 
+        FROM OrderItem oi2 
+        WHERE oi2.KegID = kg.KegID
+        AND oi2.OrderItemStateID IN (0, 1, 2, 3)
+    )
+    LIMIT quantity;
 END;
 $$;
 -- rollback DROP PROCEDURE IF EXISTS CreateOderItemsForCustomerOrder(INTEGER,INTEGER,INTEGER,INTEGER);
