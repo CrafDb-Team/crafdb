02f7fd4aabd1234a3f1499b4f478c602d5c10eda
Ryan Trickett <ryan.trickett@bbd.co.za>
2024-02-14 08:17:36 +0200

    Fixed Small Errors in Files and Changeset Identities

--- sql/references.sql
+++ sql/references.sql
@@ -6,23 +6,23 @@ ADD CONSTRAINT OrderItem_OrderItemStateID_FK FOREIGN KEY(OrderItemStateID)
     REFERENCES OrderItemState(OrderItemStateID)
 
 -- rollback ALTER TABLE OrderItem DROP CONSTRAINT OrderItem_OrderItemStateID_FK;
-=======
--- changeset rtrickett:keg-batch-fk
+
+-- changeset rtrickett:keg-batch-fk-creation
 ALTER TABLE Keg 
 ADD CONSTRAINT Keg_BatchID_FK FOREIGN KEY (BatchID) REFERENCES Batch (BatchID);
 -- rollback ALTER TABLE Keg DROP CONSTRAINT Keg_BatchID_FK
 
--- changeset rtrickett:keg-batch-fk
+-- changeset rtrickett:batch-beer-fk-creation
 ALTER TABLE Batch 
 ADD CONSTRAINT Batch_BeerID_FK FOREIGN KEY (BeerID) REFERENCES Beer (BeerID);
 -- rollback ALTER TABLE Batch DROP CONSTRAINT Batch_BeerID_FK
 
--- changeset rtrickett:keg-batch-fk
+-- changeset rtrickett:batch-batchstate-fk-creation
 ALTER TABLE Batch 
 ADD CONSTRAINT Batch_BatchStatusID_FK FOREIGN KEY (BatchStatusID) REFERENCES BatchStatus (BatchStatusID);
 -- rollback ALTER TABLE Batch DROP CONSTRAINT Batch_BatchStatusID_FK
 
---changeset ljordaan:alter-table-beer-table
+--changeset ljordaan:beer-beertype-fk-creation
 ALTER TABLE Beer
 ADD CONSTRAINT Beer_BeerTypeID_FK FOREIGN KEY (BeerTypeID)
   REFERENCES BeerType(BeerTypeID);
