7b0c8b26c73ad2f5c26b2e798ce2b727d2490447
EdwinToTheBatCave <mamphane@bbd.co.za>
2024-02-14 13:12:33 +0200

    stored procedure without output variables

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -11,9 +11,20 @@ LANGUAGE plpgsql
 AS 
 $$
 BEGIN
+    /*
+    OrderID integer NOT NULL,
+    KegID integer NOT NULL,
+    OrderItemStateID integer NOT NULL,
+    PriceAtSale numeric NOT NULL
+    */
     INSERT INTO OrderItem oi
     SELECT 
+        order_id,
+        0,
+        br.PricePerKeg
+    FROM Keg kg
+    INNER JOIN Beer br on br.BeerID = kg.BeerID
+    FETCH FIRST quantity ROWS ONLY;
 END;
 $$;
-
 -- rollback DROP PROCEDURE IF EXISTS CreateOderItemsForCustomerOrder(INTEGER,INTEGER,INTEGER,INTEGER);
