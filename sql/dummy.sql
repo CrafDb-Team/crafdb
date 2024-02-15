b16ff7c93640e6b454a72850205b9e1677026998
Luke-Bradford-BBD <luke.bradford@bbd.co.za>
2024-02-15 14:57:46 +0200

    CRAF-43: Reordered statements

--- sql/dummy.sql
+++ sql/dummy.sql
@@ -1,5 +1,33 @@
 -- liquibase formatted sql
 
+-- changeset lbradford:table-customer-insertion
+INSERT INTO 'Customer' ('CustomerID', 'Name', 'Email', 'ContactNumber') VALUES 
+(1, 'Buzzbean', 'dvasyukhnov0@devhub.com', '+352 596 515 9541'),
+(2, 'Feednation', 'stiley1@squarespace.com', '+58 869 343 1241'),
+(3, 'Thoughtstorm', 'mskudder2@shutterfly.com', '+1 940 597 9707'),
+(4, 'Gabspot', 'ddiiorio3@unicef.org', '+86 319 479 6848'),
+(5, 'Zoonoodle', 'jeveritt4@jiathis.com', '+86 678 635 4212'),
+(6, 'Vidoo', 'bwedderburn5@goo.gl', '+7 484 577 1646'),
+(7, 'Photospace', 'obarajaz6@marriott.com', '+51 415 553 9464'),
+(8, 'Voonte', 'vdyett7@omniture.com', '+351 659 477 5898'),
+(9, 'Livetube', 'tconnachan8@ucla.edu', '+356 937 829 9865'),
+(10, 'Kaymbo', 'geades9@naver.com', '+86 575 798 1979');
+-- rollback DELETE FROM TABLE "Customer";
+
+-- changeset lbradford:table-order-insertion
+INSERT INTO 'Order' ('OrderID', 'CustomerID', 'CreatedOn') VALUES 
+(1, 1, '2023-08-25'),
+(2, 1, '2023-08-16'),
+(3, 1, '2023-09-21'),
+(4, 3, '2023-07-05'),
+(5, 4, '2024-01-20'),
+(6, 5, '2023-02-20'),
+(7, 7, '2023-07-12'),
+(8, 8, '2023-11-10'),
+(9, 8, '2023-11-02'),
+(10, 10, '2023-04-29');
+-- rollback DELETE FROM TABLE "Order";
+
 -- changeset ljordaan:table-orderitemstate-insertion
 INSERT INTO 'OrderItemState' ('OrderState')
 VALUES 
@@ -41,13 +69,6 @@ VALUES
 ('Allan-Larger', 'Spicy and complex with a hint of cloves', 3, 779.59);
 -- rollback DELETE FROM TABLE "Beer";
 
--- changeset ljordaan:table-beertype-insertion
-INSERT INTO 'BeerType' ('BeerType') 
-VALUES 
-    ('Lager'),
-    ('Pale Ale'),
-    ('Pilsner');
--- rollback DELETE FROM TABLE "BeerType";
 
 -- changeset lbradford:table-batch-insertion
 INSERT INTO 'Batch' ('BeerID', 'BatchStatusID') VALUES 
@@ -60,47 +81,22 @@ INSERT INTO 'Batch' ('BeerID', 'BatchStatusID') VALUES
 (5, 6),
 (4, 4);
 
--- changeset lbradford:table-customer-insertion
-INSERT INTO 'Customer' ('CustomerID', 'Name', 'Email', 'ContactNumber') VALUES 
-(1, 'Buzzbean', 'dvasyukhnov0@devhub.com', '+352 596 515 9541'),
-(2, 'Feednation', 'stiley1@squarespace.com', '+58 869 343 1241'),
-(3, 'Thoughtstorm', 'mskudder2@shutterfly.com', '+1 940 597 9707'),
-(4, 'Gabspot', 'ddiiorio3@unicef.org', '+86 319 479 6848'),
-(5, 'Zoonoodle', 'jeveritt4@jiathis.com', '+86 678 635 4212'),
-(6, 'Vidoo', 'bwedderburn5@goo.gl', '+7 484 577 1646'),
-(7, 'Photospace', 'obarajaz6@marriott.com', '+51 415 553 9464'),
-(8, 'Voonte', 'vdyett7@omniture.com', '+351 659 477 5898'),
-(9, 'Livetube', 'tconnachan8@ucla.edu', '+356 937 829 9865'),
-(10, 'Kaymbo', 'geades9@naver.com', '+86 575 798 1979');
--- rollback DELETE FROM TABLE "Customer";
 
--- changeset lbradford:table-order-insertion
-INSERT INTO 'Order' ('OrderID', 'CustomerID', 'CreatedOn') VALUES 
-(1, 1, '2023-08-25'),
-(2, 1, '2023-08-16'),
-(3, 1, '2023-09-21'),
-(4, 3, '2023-07-05'),
-(5, 4, '2024-01-20'),
-(6, 5, '2023-02-20'),
-(7, 7, '2023-07-12'),
-(8, 8, '2023-11-10'),
-(9, 8, '2023-11-02'),
-(10, 10, '2023-04-29');
--- rollback DELETE FROM TABLE "Order";
 
 
 -- changeset lbradford:table-keg-insertion
 INSERT INTO 'Keg' ('BatchID', 'ExpiryDate') VALUES 
 (1, '2023-07-06');
-(2, '2024-01-13'),
-(2, '2024-01-18'),
+(2, '2023-01-13'),
+(2, '2023-01-18'),
 (3, '2023-03-29'),
-(3, '2023-04-29'),
-(4, '2023-09-08'),
-(5, '2023-03-22'),
-(6, '2023-03-31'),
-(7, '2023-08-25'),
-(8, '2023-11-19');
+(3, '2024-04-29'),
+(4, '2024-09-08'),
+(5, '2024-03-22'),
+(6, '2024-03-31'),
+(7, '2024-08-25'),
+(8, '2024-11-19'),
+(8, '2024-11-25');
 -- rollback DELETE FROM TABLE "Keg";
 
 -- changeset lbradford:table-orderitem-insertion
