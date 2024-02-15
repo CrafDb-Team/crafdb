bf0d0448c5c95a05e8e6b239233b6f3bc24fcf3e
EdwinToTheBatCave <mamphane@bbd.co.za>
2024-02-15 13:12:03 +0200

    added quotes

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset emokoena:func-create-count-unsold-kegs-of-beer runOnChange:true
-CREATE OR REPLACE FUNCTION FuncCountUnsoldKegsOfBeer(beer_id integer)
+CREATE OR REPLACE FUNCTION "funcCountUnsoldKegsOfBeer"(beer_id integer)
 RETURNS integer
 LANGUAGE plpgsql
 AS
@@ -12,14 +12,14 @@ BEGIN
     SELECT count(*) 
     INTO remaining_kegs_of_beer
     FROM Beer br
-    INNER JOIN Batch bt on bt.BatchID = br.BatchID
-    INNER JOIN KEG kg on bt.BatchID = kg.BatchID
-    LEFT JOIN OrderItem oi on kg.KegID = oi.KegID
-    WHERE bt.BeerID = beer_id
-    AND oi.OrderID IS NULL;
+    INNER JOIN "Batch" bt on bt."BatchID" = br."BatchID"
+    INNER JOIN "KEG" kg on bt."BatchID" = kg."BatchID"
+    LEFT JOIN "OrderItem" oi on kg."KegID" = oi."KegID"
+    WHERE bt."BeerID" = beer_id
+    AND oi."OrderID" IS NULL;
 
     RETURN remaining_kegs_of_beer;
 END;
 ';
 
--- rollback DROP FUNCTION IF EXISTS FuncCountUnsoldKegsOfBeer(integer);
+-- rollback DROP FUNCTION IF EXISTS "funcCountUnsoldKegsOfBeer"(integer);
