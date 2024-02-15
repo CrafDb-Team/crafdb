a6338e7d62f184f50d684f463f157cc94daf657f
EdwinToTheBatCave <mamphane@bbd.co.za>
2024-02-15 12:19:23 +0200

    quotes and dollar quotes

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -6,33 +6,32 @@ CREATE OR REPLACE PROCEDURE ProcCreateOderItemsForCustomerOrder(
    order_id integer,
    inout quantity integer
 )
-LANGUAGE plpgsql    
 AS 
-'
+$$
 BEGIN
-    INSERT INTO OrderItem(OrderID, KegID, OrderItemStateID, PriceAtSale)
+    INSERT INTO OrderItem("OrderID", "KegID", "OrderItemStateID", "PriceAtSale")
     SELECT 
-        order_id,
-        kg.KegID,
+        "order_id",
+        kg."KegID",
         0,
-        br.PricePerKeg
-    FROM Keg kg
-    INNER JOIN Beer br on br.BeerID = kg.BeerID
+        br."PricePerKeg"
+    FROM "Keg" kg
+    INNER JOIN "Beer" br on br."BeerID" = kg."BeerID"
     WHERE NOT EXISTS (
         SELECT 
-            oi.KegID, 
-            oi.OrderItemStateID 
-        FROM OrderItem oi 
-        WHERE oi.KegID = kg.KegID
-        AND oi.OrderItemStateID IN (0, 1, 2, 3)
-        AND kg.ExpiryDate > CURRENT_DATE
+            oi."KegID", 
+            oi."OrderItemStateID" 
+        FROM "OrderItem" oi 
+        WHERE oi."KegID" = kg."KegID"
+        AND oi."OrderItemStateID" IN (1, 2, 3, 4)
     )
-    AND kg.BeerID = beer_id
-    LIMIT quantity;
+    AND kg."BeerID" = "beer_id"
+    AND kg."ExpiryDate" > CURRENT_DATE
+    LIMIT "quantity";
 
-    GET DIAGNOSTICS quantity := ROW_COUNT;
+    GET DIAGNOSTICS "quantity" := ROW_COUNT;
     COMMIT;
 END;
-';
+$$ LANGUAGE plpgsql;   
 
 -- rollback DROP PROCEDURE IF EXISTS ProcCreateOderItemsForCustomerOrder(INTEGER,INTEGER,INTEGER);
